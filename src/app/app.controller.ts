64e175bc496705b747dc96ca57a96b879964cf16
thangnv6 <thangnv@foobla.com>
2021-05-02 11:09:12 +0700

    remove package sqlite

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,14 +1,14 @@
 import { controller, IAppController } from '@foal/core'
-import { createConnection } from 'typeorm'
+// import { createConnection } from 'typeorm'
 
 import { ApiController } from './controllers'
 
 export class AppController implements IAppController {
   subControllers = [
-    controller('/api', ApiController),
+    controller('/apia', ApiController),
   ];
 
   async init() {
-    await createConnection()
+    // await createConnection()
   }
 }
